4ba95cf405d35bfc2bc387b36a589d5d7c2be9b8
haydenpt <hayden.pt1610@gmail.com>
2023-01-07 05:56:34 -0500

    Add basic auth

---
+++ src/main/java/com/misso/votingappbackend/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.misso.votingappbackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests((requests) -> requests
+                .anyRequest().authenticated()
+                )
+                .formLogin().disable()
+                .httpBasic();
+        return http.build();
+    }
+
+// TODO: create UserService
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user =
+//                User.withDefaultPasswordEncoder()
+//                        .username("user")
+//                        .password("password")
+//                        .roles("USER")
+//                        .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
+}
--- src/main/java/com/misso/votingappbackend/controller/PositionController.java
+++ src/main/java/com/misso/votingappbackend/controller/PositionController.java
@@ -3,6 +3,7 @@ package com.misso.votingappbackend.controller;
 import com.misso.votingappbackend.entity.Position;
 import com.misso.votingappbackend.service.PositionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,36 +19,8 @@ public class PositionController {
 
     @GetMapping("/get-all")
     public Iterable<Position> getAllPositions() {
+        System.out.println(SecurityContextHolder.getContext().getAuthentication());
         return positionService.getAllPositions();
     }
 
-//    @GetMapping("/get-all")
-//    public ArrayList<Position> getAllPositions() {
-//        ArrayList<Position> positions = new ArrayList<>();
-//        positions.add(new Position("tech",
-//                "Technology",
-//                "Education",
-//                "Team Lead",
-//                "settings",
-//                "https://misso.org"));
-//        positions.add(new Position("java",
-//                "Java",
-//                "Education",
-//                "Officer",
-//                "local_cafe",
-//                "https://misso.org"));
-//        positions.add(new Position("data",
-//                "Database",
-//                "Education",
-//                "Officer",
-//                "local_cafe",
-//                "https://misso.org"));
-//        positions.add(new Position("tran",
-//                "Transaction Processing",
-//                "Education",
-//                "Officer",
-//                "local_cafe",
-//                "https://misso.org"));
-//        return positions;
-//    }
 }
